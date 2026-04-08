27e2fb442980a57c3f237395b7d41aedac0ba508
twistedlogic <bronan.dev@proton.me>
2026-04-08 00:00:00 +0000

    fix: .txt.html -> .html

--- build/postprocessing.sh
+++ build/postprocessing.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 # A simple script that converts certain files to html and runs prettier on them
-for f in 06_MainDefinition.txt 08_Sentence.txt 09_SentenceFurigana.txt 11_Picture.txt 12_Glossary.txt 18_PitchPosition.txt 20_Frequency.txt; do 
-  cp -- "$f" "$f.html"; prettier "$f.html" > "$f.2.html"; rm "$f" "$f.html"; mv "$f.2.html" "$f.html"
+for f in ./example_card/06_MainDefinition.txt ./example_card/08_Sentence.txt ./example_card/09_SentenceFurigana.txt ./example_card/11_Picture.txt ./example_card/12_Glossary.txt ./example_card/18_PitchPosition.txt ./example_card/20_Frequency.txt; do 
+  base="${f%.*}"
+  cp -- "$f" "$base.html"
+  prettier "$base.html" > "$base.2.html"
+  rm "$f" "$base.html"
+  mv "$base.2.html" "$base.html"
 done

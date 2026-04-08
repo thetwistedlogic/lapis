dfe15e1c48f9969ed7649cfdd123caadbfa11e85
twistedlogic <bronan.dev@proton.me>
2026-04-08 00:00:00 +0000

    chore: use js-beautify for formatting instead

--- build/postprocessing.sh
+++ build/postprocessing.sh
@@ -3,7 +3,8 @@
 for f in ./example_card/06_MainDefinition.txt ./example_card/08_Sentence.txt ./example_card/09_SentenceFurigana.txt ./example_card/11_Picture.txt ./example_card/12_Glossary.txt ./example_card/18_PitchPosition.txt ./example_card/20_Frequency.txt; do 
   base="${f%.*}"
   cp -- "$f" "$base.html"
-  prettier "$base.html" > "$base.2.html"
+  # npm i -g js-beautify
+  js-beautify "$base.html" > "$base.2.html"
   rm "$f" "$base.html"
   mv "$base.2.html" "$base.html"
 done
